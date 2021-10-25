3c5b92d75d4ee21d11fe90b26a496fedf710d8d6
ainoylaoutinen <bgp962@myy.haaga-helia.fi>
2021-10-25 11:33:20 +0300

    database

--- src/main/java/com/example/bookstore2/domain/User.java
+++ src/main/java/com/example/bookstore2/domain/User.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="usertable")
 public class User {
 
     @Id
@@ -17,7 +19,7 @@ public class User {
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "passwordHash", nullable = false)
     private String passwordHash;
     
     @Column(name = "email", nullable = false)
